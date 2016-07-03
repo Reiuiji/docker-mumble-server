ce2555c6f0f402628325f936826b68c305e91e80
Reiuiji <reiuiji@gmail.com>
2016-07-03 03:05:14 -0400

    Updated init for main location I usually use

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Change to location of mumble-server data location
-datadir="../mumble-server"
+datadir="/opt/docker-svr/mumble-server"
 docker run --name=mumble-server -d --net=host -v $datadir:/data -t coppit/mumble-server
